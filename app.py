b979dedf7c1643698c60a756aac5b006bb642c59
kembudivya <kembudivya@gmail.com>
2026-03-05 15:29:20 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+import os
+
+app = Flask(__name__)
+
+LOG_FILE = "logs/sample.log"
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    errors = []
+
+    if request.method == 'POST':
+        keyword = request.form['keyword']
+
+        with open(LOG_FILE, 'r') as file:
+            for line in file:
+                if keyword.lower() in line.lower():
+                    errors.append(line.strip())
+        if os.path.exists(LOG_FILE):
+            with open(LOG_FILE, 'r') as file:
+                for line in file:
+                    if keyword.lower() in line.lower():
+                        errors.append(line.strip())
+        else:
+            errors.append("Log file not found.")
+
+    return render_template('index.html', errors=errors)
+
+
+if __name__ == "__main__":
